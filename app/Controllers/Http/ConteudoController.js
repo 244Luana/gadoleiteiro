eef167dda0e29c84ada375c3bf28a931cc37eb81
Luana Amaral <244luana@gmail.com>
2020-12-11 17:00:55 -0300

    fe

---
+++ app/Controllers/Http/ConteudoController.js
@@ -0,0 +1,42 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Conteudo = use ('App/Models/Conteudo')
+
+/**
+ * Resourceful controller for interacting with conteudos
+ */
+class ConteudoController {
+  /**
+   * Show a list of all conteudos.
+   * GET conteudos
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+    const conts = await Conteudo.all()
+    return conts
+  }
+
+  /**
+   * Display a single conteudo.
+   * GET conteudos/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+    const conts = await Conteudo.findOrFail(params.id)
+    return conts
+  }
+
+}
+
+module.exports = ConteudoController
---
+++ app/Controllers/Http/ForumController.js
@@ -0,0 +1,98 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Forum = use ('App/Models/Forum')
+
+/**
+ * Resourceful controller for interacting with forums
+ */
+class ForumController {
+  /**
+   * Show a list of all forums.
+   * GET forums
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+    const forums = await Forum.all()
+    return forums 
+  }
+ 
+  /**
+   * Create/save a new forum.
+   * POST forums
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response, auth }) {
+    const {conteudo, resposta} = request.only(["conteudo", "resposta"])
+    const forums = await Forum.create({conteudo, resposta, id_user:auth.user.id})
+    return forums
+  }
+
+  /**
+   * Display a single forum.
+   * GET forums/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+    const forums = await Forum.findOrFail(params.id)
+    return forums 
+  }
+
+  /**
+   * Render a form to update an existing forum.
+   * GET forums/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update forum details.
+   * PUT or PATCH forums/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+    const forums = await Forum.findOrFail(params.id)
+    const{conteudo, resposta} = request.only (["conteudo", "resposta"])
+    forums.conteudo = conteudo
+    forums.resposta = resposta
+    await forums.save()
+    return forums 
+  }
+
+  /**
+   * Delete a forum with id.
+   * DELETE forums/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+    const forums = await Forum.findOrFail(params.id)
+    await forums.delete()
+    return forums
+  }
+}
+
+module.exports = ForumController
